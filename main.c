5a754c1cb757ab0085ad88872978ef26e3752b92
lovaszimarci <lovaszimarci@gmail.com>
2026-05-09 18:46:47 +0200

    lcd init and lcd set cursor

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <avr/interrupt.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <util/delay.h>
 
 // LCD es I2C makrok
 /*
@@ -275,17 +276,88 @@ void LCD_PrintString(const char* str){
 }
 
 
+void LCD_Init(){
+
+
+    // az lcd kijelzőt 4 bites működésre a HD44780 datasheet alapján programoztam fel
+    // a datasheet tartalmazza az osszes itt talalhato magic numbert
+    //https://www.alldatasheet.com/datasheet-pdf/download/63673/HITACHI/HD44780.html
+
+    // tapfesz utani delay
+    _delay_ms(50);
+
+    //elso ebresztes, datasheet alapjan 0x03
+    LCD_PulseEnable( (0x03 << 4) | LCD_BACKLIGHT );
+    _delay_ms(5);
+
+    //masodik ebresztes
+    LCD_PulseEnable( (0x03 << 4) | LCD_BACKLIGHT );
+    _delay_us(150);
+
+    //harmadik ebresztes
+    LCD_PulseEnable( (0x03 << 4) | LCD_BACKLIGHT );
+    _delay_us(150);
+
+    //atkapcsolas 4 bites modba
+    LCD_PulseEnable( (0x02 << 4) | LCD_BACKLIGHT );
+    _delay_ms(1);
+
+    // mivel mostmar 4 bites modban van a kijelzo igy lehet hasznalni a sajat fugvenyeket
+
+    //kijelzo parameterinek allitasa: 4 bites mod, 2 sor, 5x8-as font ()
+    LCD_SendCommand(0x28);
+    _delay_ms(1);
+
+    // kijelzo kikapcsolasa, ez biztonsagi lepes a torles elott
+    LCD_SendCommand(0x08);
+    _delay_ms(1);
+
+    // kepernyo teljes torlese
+    LCD_SendCommand(0x01);
+    _delay_ms(3);
+
+    // a kurzor beviteli modja: mindig lepjen egyet jobbra
+    LCD_SendCommand(0x06);
+    _delay_ms(1);
+
+    // kijelzo visszakapcsolasa es kurzor elrejtese
+    LCD_SendCommand(0x0C);
+    _delay_ms(1);
+
+}
+
+//LCD kurzor mozgatasa
+// 1. sor cime: 0x00
+// 2. sor cime: 0x40
+// kurzor mozgato parancs: 0x80
+// pl masodik sorba mozgatas: 0x80 + 0x40 = 0xC0
+void LCD_SetCursor(uint8_t row, uint8_t col){
+    uint8_t addres;
+    if(row == 0){
+        addres = 0x80 + col;
+    }
+    else{
+        addres = 0xC0 + col;
+    }
+    LCD_SendCommand(addres);
+}
+
+
+
+
 
 
 int main(){
     //hw setup
+    I2C_init();
+    LCD_Init();
     SetupTimer();
     SetupComp();
 
+
+
     while(true){
         switch(GlobalState){
-
-            // SYNC fázis
 ///////////////////////////////////////////////////////////////////////////
             case SYNC:
                 //ha van uj tuske
